db92546aa1c7c73a656614829dbe6b720a796899
Eduardo Becker <edu.bkr@gmail.com>
2019-10-10 15:09:32 -0300

    feature: load the correct option in the oneOf and always clear the data when the type is changed

--- src/components/fields/MultiSchemaField.js
+++ src/components/fields/MultiSchemaField.js
@@ -4,10 +4,8 @@ import * as types from "../../types";
 import {
   getUiOptions,
   getWidget,
-  guessType,
-  retrieveSchema,
   getDefaultFormState,
-  getMatchingOption
+  getMatchingOption,
 } from "../../utils";
 
 class AnyOfField extends Component {
@@ -17,9 +15,27 @@ class AnyOfField extends Component {
     const { formData, options } = this.props;
 
     this.state = {
-      selectedOption: this.getMatchingOption(formData, options)
+      selectedOption: this.getMatchingOption(formData, options),
     };
   }
+
+  getCurrentOption = (formData, options) => {
+    const { __type } = formData;
+    const ref = __type.replace(".", "#/definitions/");
+
+    let index = 0;
+
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+
+      if (option.$ref === ref) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  };
 
   UNSAFE_componentWillReceiveProps(nextProps) {
     const matchingOption = this.getMatchingOption(
@@ -37,55 +53,36 @@ class AnyOfField extends Component {
   getMatchingOption(formData, options) {
     const { definitions } = this.props.registry;
 
-    let option = getMatchingOption(formData, options, definitions);
-    if (option !== 0) {
-      return option;
+    if (formData && formData.__type) {
+      return this.getCurrentOption(formData, options);
+    } else {
+      const option = getMatchingOption(formData, options, definitions);
+
+      if (option !== 0) {
+        return option;
+      }
+      // If the form data matches none of the options, use the currently selected
+      // option, assuming it's available; otherwise use the first option
+      return this && this.state ? this.state.selectedOption : 0;
     }
-    // If the form data matches none of the options, use the currently selected
-    // option, assuming it's available; otherwise use the first option
-    return this && this.state ? this.state.selectedOption : 0;
   }
 
   onOptionChange = option => {
     const selectedOption = parseInt(option, 10);
-    const { formData, onChange, options, registry } = this.props;
+    const { onChange, options, registry } = this.props;
     const { definitions } = registry;
-    const newOption = retrieveSchema(
-      options[selectedOption],
-      definitions,
-      formData
-    );
 
     // If the new option is of type object and the current data is an object,
     // discard properties added using the old option.
     let newFormData = undefined;
-    if (
-      guessType(formData) === "object" &&
-      (newOption.type === "object" || newOption.properties)
-    ) {
-      newFormData = Object.assign({}, formData);
 
-      const optionsToDiscard = options.slice();
-      optionsToDiscard.splice(selectedOption, 1);
-
-      // Discard any data added using other options
-      for (const option of optionsToDiscard) {
-        if (option.properties) {
-          for (const key in option.properties) {
-            if (newFormData.hasOwnProperty(key)) {
-              delete newFormData[key];
-            }
-          }
-        }
-      }
-    }
     // Call getDefaultFormState to make sure defaults are populated on change.
     onChange(
       getDefaultFormState(options[selectedOption], newFormData, definitions)
     );
 
     this.setState({
-      selectedOption: parseInt(option, 10)
+      selectedOption: parseInt(option, 10),
     });
   };
 
@@ -104,7 +101,7 @@ class AnyOfField extends Component {
       options,
       registry,
       safeRenderCompletion,
-      uiSchema
+      uiSchema,
     } = this.props;
 
     const _SchemaField = registry.fields.SchemaField;
@@ -126,7 +123,7 @@ class AnyOfField extends Component {
 
     const enumOptions = options.map((option, index) => ({
       label: option.title || `Option ${index + 1}`,
-      value: index
+      value: index,
     }));
 
     return (
@@ -170,7 +167,7 @@ AnyOfField.defaultProps = {
   disabled: false,
   errorSchema: {},
   idSchema: {},
-  uiSchema: {}
+  uiSchema: {},
 };
 
 if (process.env.NODE_ENV !== "production") {
@@ -181,7 +178,7 @@ if (process.env.NODE_ENV !== "production") {
     idSchema: PropTypes.object,
     formData: PropTypes.any,
     errorSchema: PropTypes.object,
-    registry: types.registry.isRequired
+    registry: types.registry.isRequired,
   };
 }
 
