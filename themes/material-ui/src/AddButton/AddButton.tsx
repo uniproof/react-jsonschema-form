aedf37d3beb89a498b367177afa8b8aea31a4f74
jadson <jadson.soares@e-juno.com.br>
2020-11-13 13:58:27 -0300

    change the add button label

--- themes/material-ui/src/AddButton/AddButton.tsx
+++ themes/material-ui/src/AddButton/AddButton.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import { AddButtonProps } from 'react-jsonschema-form';
+import { AddButtonProps } from "@rjsf/core";
 
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
 
 const AddButton: React.FC<AddButtonProps> = props => (
   <Button {...props} color="secondary">
-    <AddIcon /> Add Item
+    <AddIcon /> Adicionar
   </Button>
 );
 
